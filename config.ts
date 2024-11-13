95a31315925f6a2b5410a571b31d28d84cd7c910
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2024-11-13 20:50:12 +0100

    replace single quotes with double quotes

--- config.ts
+++ config.ts
@@ -1,10 +1,11 @@
 export const config = {
     logLevel: 'info',
+    logFile: "app.log",
     api: {
-        baseURL: 'http://localhost/api'
+        baseURL: "http://localhost/api"
     },
     context: {
-        baseURI: 'http://example.globalise.nl/temp',
+        baseURI: "http://example.globalise.nl/temp",
         uniqueField: ["id", "type"]
     }
 }
